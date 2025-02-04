21049c02b082785f4280dba3ea61eba4b9b54471
edson-araujo <edson-456@hotmail.com>
2025-02-03 21:30:53 -0300

    first commit

---
+++ src/test/java/com/wave/porquinho/PorquinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.porquinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorquinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
